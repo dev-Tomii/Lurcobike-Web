a740f6bf99ef5ad45290b9e084043da0843d94f2
Tomii Medina <dev.tomii03@gmail.com>
2024-07-03 16:23:37 -0300

    fixing home

--- src/components/ImageSlide.tsx
+++ src/components/ImageSlide.tsx
@@ -10,23 +10,27 @@ import { ImGift } from "react-icons/im";
 
 const images = [
 	{
-		src: "/imgs/logoimg.png",
+		src: "/imgs/imglogo.png",
 		alt: "Lurcobike Logo",
+		type: "0",
 		desc: "0",
 	},
 	{
 		src: "/imgs/asiento/1.png",
 		alt: "Imagen",
+		type: "Asiento",
 		desc: "Lurcobike",
 	},
 	{
 		src: "/imgs/seguridad/4.png",
 		alt: "Imagen",
+		type: "Candado de seguridad",
 		desc: "Traba U",
 	},
 	{
 		src: "/imgs/portaequipaje/2.png",
 		alt: "Imagen",
+		type: "Portaequipaje",
 		desc: "Aluminio",
 	},
 ];
@@ -44,26 +48,26 @@ export default function ImageSlide() {
 					disableOnInteraction: true,
 				}}
 				loop={true}
-				className="bg-transparent rounded-md"
+				className="rounded-md"
 			>
 				{images.map((image, index) => (
 					<SwiperSlide key={index} className="rounded-lg">
 						<Card
 								radius="md"
 								isFooterBlurred
-								className={image.desc != "0" ? "bg-black/10 h-[15em] xl:h-[50em] flex items-center object-fill" : "bg-black h-[15em] xl:h-[50em] flex items-center object-fill"}
+								className={image.desc != "0" ? "bg-black/10 h-[18em] xl:h-[50em] flex items-center object-fill" : "bg-black h-[18em] xl:h-[50em] flex items-center object-fill"}
 							>
 								<Image
 									src={image.src}
 									alt="asd"
 									width={1500}
-									className="mx-auto h-[15em] xl:h-[50em] object-cover"
+									className="mx-auto h-[18em] xl:h-[50em] object-contain"
 									radius="md"
 								></Image>
 								{image.desc != "0" && (
 									<CardFooter className="absolute bottom-0 z-10 bg-black/30 border-t-1 border-zinc-100/50 flex flex-col justify-center items-center pb-8">
 										<h1 className="text-base xl:text-xl uppercase font-bold text-amber-400">
-											Mas Vendido
+											{image.type}
 										</h1>
 										<h2 className="text-tiny font-sm uppercase">
 											{image.desc}
