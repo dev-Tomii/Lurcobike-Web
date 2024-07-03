74dc83c8eb9e4ec6b4e0df1e79a1c33d0ef305b9
Tomii Medina <dev.tomii03@gmail.com>
2024-07-03 16:06:46 -0300

    replaced home images

--- src/components/ImageSlide.tsx
+++ src/components/ImageSlide.tsx
@@ -5,16 +5,35 @@ import "swiper/css";
 
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css/pagination";
+import { Card, CardFooter, Skeleton } from "@nextui-org/react";
+import { ImGift } from "react-icons/im";
 
-export const images = [
-	{ src: "/imgs/test.png", alt: "Imagen" },
-	{ src: "/imgs/test.png", alt: "Imagen" },
-	{ src: "/imgs/test.png", alt: "Imagen" },
+const images = [
+	{
+		src: "/imgs/logoimg.png",
+		alt: "Lurcobike Logo",
+		desc: "0",
+	},
+	{
+		src: "/imgs/asiento/1.png",
+		alt: "Imagen",
+		desc: "Lurcobike",
+	},
+	{
+		src: "/imgs/seguridad/4.png",
+		alt: "Imagen",
+		desc: "Traba U",
+	},
+	{
+		src: "/imgs/portaequipaje/2.png",
+		alt: "Imagen",
+		desc: "Aluminio",
+	},
 ];
 
 export default function ImageSlide() {
 	return (
-		<div className="container mx-auto my-7 w-[90%] min-h-[11em]">
+		<div className="container mx-auto my-7 w-[90%] min-h-[11em] rounded-md">
 			<Swiper
 				slidesPerView={1}
 				modules={[Pagination, Autoplay]}
@@ -25,16 +44,33 @@ export default function ImageSlide() {
 					disableOnInteraction: true,
 				}}
 				loop={true}
+				className="bg-transparent rounded-md"
 			>
 				{images.map((image, index) => (
-					<SwiperSlide key={index}>
-						<div className="flex h-full w-full items-center justify-center">
-							<Image
-								src={image.src}
-								alt={image.alt}
-								className="object-cover"
-							></Image>
-						</div>
+					<SwiperSlide key={index} className="rounded-lg">
+						<Card
+								radius="md"
+								isFooterBlurred
+								className={image.desc != "0" ? "bg-black/10 h-[15em] xl:h-[50em] flex items-center object-fill" : "bg-black h-[15em] xl:h-[50em] flex items-center object-fill"}
+							>
+								<Image
+									src={image.src}
+									alt="asd"
+									width={1500}
+									className="mx-auto h-[15em] xl:h-[50em] object-cover"
+									radius="md"
+								></Image>
+								{image.desc != "0" && (
+									<CardFooter className="absolute bottom-0 z-10 bg-black/30 border-t-1 border-zinc-100/50 flex flex-col justify-center items-center pb-8">
+										<h1 className="text-base xl:text-xl uppercase font-bold text-amber-400">
+											Mas Vendido
+										</h1>
+										<h2 className="text-tiny font-sm uppercase">
+											{image.desc}
+										</h2>
+									</CardFooter>
+								)}
+							</Card>
 					</SwiperSlide>
 				))}
 			</Swiper>
